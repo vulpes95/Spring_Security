1a300fd66fcc35416054559cf0a9ee84300d2ab2
Anton Liseytsev <antonlis2@yandex.ru>
2018-10-07 12:17:51 +0300

    add jdbc database authentication

--- src/main/java/eustrosoft/org/config/Config.java
+++ src/main/java/eustrosoft/org/config/Config.java
@@ -1,17 +1,32 @@
 package eustrosoft.org.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="eustrosoft.org")
+@PropertySource("classpath:persistence-mysql.properties")
 public class Config {
+	
+	@Autowired
+	private Environment env;
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	@Bean
 	public ViewResolver viewResolver() {
@@ -20,4 +35,49 @@ public class Config {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	
+	@Bean
+	public DataSource secuityDataSource() {
+		
+		ComboPooledDataSource secuityDataSource = new ComboPooledDataSource();
+		try {
+			secuityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		logger.info(">>> jdbc.url="+ env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user="+ env.getProperty("jdbc.user"));
+		
+		secuityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		secuityDataSource.setUser(env.getProperty("jdbc.user"));
+		secuityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		secuityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		secuityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		secuityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		secuityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+		return secuityDataSource;
+	}
+	
+	private int getIntProperty(String propName) {
+		String propVal = env.getProperty(propName);
+		int intPropVal = Integer.parseInt(propVal);
+		return intPropVal;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/eustrosoft/org/config/SecurityConfig.java
+++ src/main/java/eustrosoft/org/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package eustrosoft.org.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,14 +15,18 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSourse;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		/*
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		auth.inMemoryAuthentication()
 			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
 			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));*/
+		auth.jdbcAuthentication().dataSource(securityDataSourse);
 	}
 
 	@Override
